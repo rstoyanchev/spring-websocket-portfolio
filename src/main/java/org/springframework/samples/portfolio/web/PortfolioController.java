6241f7547f775d22cfb80c8f1d33a9e57e812977
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-07-11 13:50:21 -0400

    Add trade execution dialog

--- src/main/java/org/springframework/samples/portfolio/web/PortfolioController.java
+++ src/main/java/org/springframework/samples/portfolio/web/PortfolioController.java
@@ -4,27 +4,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.messaging.annotation.MessageMapping;
 import org.springframework.samples.portfolio.PortfolioPosition;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.messaging.annotation.SubscribeEvent;
-import org.springframework.web.messaging.support.WebMessagingTemplate;
 
 
 @Controller
 public class PortfolioController {
 
-	private final WebMessagingTemplate messagingTemplate;
-
-
-	@Autowired
-	public PortfolioController(WebMessagingTemplate messagingTemplate) {
-		this.messagingTemplate = messagingTemplate;
-	}
+	private static final Log logger = LogFactory.getLog(PortfolioController.class);
 
 
 	@SubscribeEvent("/positions")
 	public List<PortfolioPosition> getPortfolios() throws IOException {
+
 		List<PortfolioPosition> positions = new ArrayList<PortfolioPosition>();
 		positions.add(new PortfolioPosition("Citrix Systems, Inc.", "CTXS", 24.30, 75));
 		positions.add(new PortfolioPosition("Dell Inc.", "DELL", 13.44, 50));
@@ -32,7 +28,16 @@ public class PortfolioController {
 		positions.add(new PortfolioPosition("Google Inc", "GOOG", 905.09, 5));
 		positions.add(new PortfolioPosition("Microsoft", "MSFT", 34.15, 33));
 		positions.add(new PortfolioPosition("VMware, Inc.", "VMW", 65.58, 23));
+
 		return positions;
+	}
+
+	@MessageMapping("/tradeRequest")
+	public void executeTrade(TradeRequest tradeRequest) {
+
+		logger.debug("Trade request: " + tradeRequest);
+
+		// TODO: execute
 	}
 
 }
--- src/main/webapp/assets/js/portfolio-viewModel.js
+++ src/main/webapp/assets/js/portfolio-viewModel.js
@@ -1,4 +1,43 @@
 
+function PortfolioModel(client) {
+	var self = this;
+
+	self.portfolioRows = ko.observableArray();
+	self.lookup = {};
+
+	self.totalShares = ko.computed(function() {
+		var result = 0;
+		for ( var i = 0; i < self.portfolioRows().length; i++) {
+			result += self.portfolioRows()[i].shares();
+		}
+		return result;
+	});
+
+	self.totalValue = ko.computed(function() {
+		var result = 0;
+		for ( var i = 0; i < self.portfolioRows().length; i++) {
+			result += self.portfolioRows()[i].value();
+		}
+		return "$" + result.toFixed(2);
+	});
+
+	self.loadPositions = function(positions) {
+		for ( var i = 0; i < positions.length; i++) {
+			var row = new PortfolioRow(positions[i]);
+			self.portfolioRows.push(row);
+			self.lookup[row.ticker] = row;
+		}
+	};
+
+	self.processQuote = function(quote) {
+		if (self.lookup.hasOwnProperty(quote.ticker)) {
+			self.lookup[quote.ticker].updatePrice(quote.price);
+		}
+	};
+
+	self.trade = ko.observable(new TradeAction(client));
+};
+
 function PortfolioRow(position) {
 	var self = this;
 
@@ -20,46 +59,39 @@ function PortfolioRow(position) {
 
 	self.updatePrice = function(newPrice) {
 		var delta = (newPrice - self.price()).toFixed(2);
-		self.arrow((delta < 0) ? '<i class="icon-arrow-down"></i>'
-				: '<i class="icon-arrow-up"></i>');
+		self.arrow((delta < 0) ? '<i class="icon-arrow-down"></i>' : '<i class="icon-arrow-up"></i>');
 		self.change((delta / self.price() * 100).toFixed(2));
 		self.price(newPrice);
 	};
 };
 
-function PortfolioViewModel() {
+function TradeAction(client) {
 	var self = this;
 
-	self.portfolioRows = ko.observableArray();
-	self.lookup = {};
+	self.action = ko.observable();
+	self.selectedRow = ko.observable({});
+	self.shares = ko.observable(0);
 
-	self.loadPositions = function(positions) {
-		for ( var i = 0; i < positions.length; i++) {
-			var row = new PortfolioRow(positions[i]);
-			self.portfolioRows.push(row);
-			self.lookup[row.ticker] = row;
-		}
+	self.showBuy = function(row) { 
+		self.selectedRow(row);
+		self.action('Buy');
+		$('#tradeDialog').modal();
 	};
 
-	self.processQuote = function(quote) {
-		if (self.lookup.hasOwnProperty(quote.ticker)) {
-			self.lookup[quote.ticker].updatePrice(quote.price);
-		}
+	self.showSell = function(row) { 
+		self.selectedRow(row);
+		self.action('Sell');
+		$('#tradeDialog').modal();
 	};
 
-	self.totalShares = ko.computed(function() {
-		var result = 0;
-		for ( var i = 0; i < self.portfolioRows().length; i++) {
-			result += self.portfolioRows()[i].shares();
-		}
-		return result;
-	});
-
-	self.totalValue = ko.computed(function() {
-		var result = 0;
-		for ( var i = 0; i < self.portfolioRows().length; i++) {
-			result += self.portfolioRows()[i].value();
-		}
-		return "$" + result.toFixed(2);
-	});
-};
+	self.executeTrade = function() {
+		var request = {
+				"ticker" : self.selectedRow().ticker,
+				"shares" : self.shares(),
+				"action" : self.action()
+			};
+		console.log(request);
+		client.send("/tradeRequest", {}, JSON.stringify(request));
+		$('#tradeDialog').modal('hide');
+	}
+}
