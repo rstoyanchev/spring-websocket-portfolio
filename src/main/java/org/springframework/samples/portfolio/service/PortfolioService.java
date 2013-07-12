3d92a19a879b34d4e319f1f06dfce07b56d7912b
Rob Winch <rwinch@vmware.com>
2013-07-12 10:36:31 -0500

    Add PortfolioService for per user Portfolios

---
+++ src/main/java/org/springframework/samples/portfolio/service/PortfolioService.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.portfolio.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.samples.portfolio.Portfolio;
+
+/**
+ * @author Rob Winch
+ *
+ */
+public class PortfolioService {
+
+	private final Map<String, Portfolio> usernameToPortfolio = new HashMap<>();
+	
+	public void setPortfolio(String username, Portfolio portfolio) {
+		this.usernameToPortfolio.put(username, portfolio);
+	}
+
+	public Portfolio findPortfolio(String username) {
+		return usernameToPortfolio.get(username);
+	}
+}
