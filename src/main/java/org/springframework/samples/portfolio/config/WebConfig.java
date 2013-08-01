5e0af2a9922cdae0b14230436be332c82550b176
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-07-31 22:00:03 -0400

    Update to latest Spring Framework 4.0 snapshot changes

--- src/main/java/org/springframework/samples/portfolio/config/WebConfig.java
+++ src/main/java/org/springframework/samples/portfolio/config/WebConfig.java
@@ -27,9 +27,9 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.socket.sockjs.SockJsHttpRequestHandler;
 import org.springframework.web.socket.sockjs.SockJsService;
-import org.springframework.web.socket.sockjs.support.DefaultSockJsService;
-import org.springframework.web.socket.sockjs.support.SockJsHttpRequestHandler;
+import org.springframework.web.socket.sockjs.transport.handler.DefaultSockJsService;
 
 
 @Configuration
