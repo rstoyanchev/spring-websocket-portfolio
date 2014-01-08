f79d3754cbd99b66bd413928822f21de907bfbf3
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2014-01-08 17:08:38 -0500

    Upgrade to Spring Framework 4.0.1.BUILD-SNAPSHOT

--- src/main/java/org/springframework/samples/portfolio/config/WebSocketConfig.java
+++ src/main/java/org/springframework/samples/portfolio/config/WebSocketConfig.java
@@ -2,19 +2,24 @@ package org.springframework.samples.portfolio.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.converter.SimpleMessageConverter;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
 @EnableScheduling
 @ComponentScan(basePackages="org.springframework.samples")
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
