4c22c48e93d2f26a0b9f459dd0c58b2410730535
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2014-03-25 00:16:01 -0400

    Upgrade to stomp.js 2.3.1

--- src/main/java/org/springframework/samples/portfolio/config/WebConfig.java
+++ src/main/java/org/springframework/samples/portfolio/config/WebConfig.java
@@ -1,8 +1,6 @@
 package org.springframework.samples.portfolio.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
