024195eea3e258e45529d3bbe9df29cb22e3930f
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2016-03-07 16:36:46 -0500

    Add WebJarsResourceResolver

--- src/main/java/org/springframework/samples/portfolio/config/WebConfig.java
+++ src/main/java/org/springframework/samples/portfolio/config/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
 
 @Configuration
@@ -18,6 +20,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/")
+			.resourceChain(false)
+				.addResolver(new WebJarsResourceResolver())
+				.addResolver(new PathResourceResolver());
 	}
 }
