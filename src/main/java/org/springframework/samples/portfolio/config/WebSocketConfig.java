a98527c024a4de6b461d20770c3e5d96e541b44a
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2018-05-16 11:43:45 -0400

    Update SFW and SEC versions

--- src/main/java/org/springframework/samples/portfolio/config/WebSocketConfig.java
+++ src/main/java/org/springframework/samples/portfolio/config/WebSocketConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableScheduling
 @ComponentScan("org.springframework.samples")
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
